ec5d291b733a0473f3f055e85d79dc66a9729c96
CantinP <cantin.poiseau@gmail.com>
2026-05-13 01:29:40 +0200

    feat: DonationController avec donations

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\SiteSetting;
 use App\Models\Section;
 use App\Models\SocialLink;
+use App\Models\Donation;
 
 class DonationController extends Controller
 {
     public function index()
     {
-        $kofi    = SiteSetting::get('kofi_url', 'https://ko-fi.com/cantin');
-        $tipeee  = SiteSetting::get('tipeee_url', 'https://fr.tipeee.com/cantin');
-        $section = Section::getBySlug('donation_text');
-        $socials = SocialLink::visible()->navbar()->ordered()->get();
-        return view('donation', compact('kofi', 'tipeee', 'section', 'socials'));
+        $kofi      = SiteSetting::get('kofi_url', 'https://ko-fi.com/cantin');
+        $tipeee    = SiteSetting::get('tipeee_url', 'https://fr.tipeee.com/cantin');
+        $section   = Section::getBySlug('donation_text');
+        $socials   = SocialLink::visible()->navbar()->ordered()->get();
+        $donations = Donation::latest()->take(20)->get();
+        return view('donation', compact('kofi', 'tipeee', 'section', 'socials', 'donations'));
     }
 }
