40e36a7a66181eced9a85c4e03c2891890a9dd53
CantinP <cantin.poiseau@gmail.com>
2026-05-13 01:29:37 +0200

    feat: Model Donation

---
+++ app/Models/Donation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    protected $fillable = [
+        'platform',
+        'username',
+        'amount',
+        'currency',
+        'message',
+    ];
+}
